ed2c46ecc3c600e0d4cfa9039d1b91d26f776d6f
Naveen055 <127019704+Naveen055@users.noreply.github.com>
2025-05-20 07:42:41 +0530

    Update Ques_1.jsx

--- src/Module 3/Introduction to ReactJS/Ques_1.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_1.jsx
@@ -1,5 +1,11 @@
-// JSX Syntax Basics
-// Description: Write basic JSX that displays "Hello, React!"
-// Use this component in App.js
+import React from 'react';
 
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+    </div>
+  );
+}
 
+export default App;
